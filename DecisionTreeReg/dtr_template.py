869b47e00386d4b30bb1e3cbcdef88ce9105cb81
lancezlin <linzhengzhong@icloud.com>
2016-12-21 00:21:42 -0800

    adding lc data from web and initiate analysis py

---
+++ DecisionTreeReg/dtr_template.py
@@ -0,0 +1,12 @@
+# libraries needed
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Importing data set
+dataset = pd.read_csv('')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+#
