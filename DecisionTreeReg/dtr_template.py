af161aca0c812206df9980c7efc75a3718af15d9
lancezlin <linzhengzhong@icloud.com>
2016-12-24 23:19:23 -0800

    reading lc data

--- DecisionTreeReg/dtr_template.py
+++ DecisionTreeReg/dtr_template.py
@@ -2,10 +2,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
+from StringIO import StringIO
 
 # Importing data set
-dataset = pd.read_csv('')
+datasetA = pd.read_csv('lc_issue_a.csv', skiprows = [0])
+datasetB = pd.read_csv('lc_rej_a.csv', skiprows = [0])
+dataset = pd.concat([datasetA, datasetB], axis = 0)
+
+
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
