c1af1d82eb85c860a34fb3e68a7f15ec58aed69f
lancezlin <linzhengzhong@icloud.com>
2016-12-28 23:48:18 -0800

    fit logistic regression model to lc data

--- LogisticReg/logreg_template.py
+++ LogisticReg/logreg_template.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 28 23:02:47 2016
+
+@author: lancel
+"""
+
 # libraries needed
 import pandas as pd
 import numpy as np
@@ -5,7 +12,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import Imputer
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.cross_validation import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 # from StringIO import StringIO
 # import global_functions
 
@@ -50,48 +57,39 @@ y = labelencoder_y.fit_transform(y)
 # split into train & test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-# feature scaling transformation
+# feature scaling transformation - two methods (minmax vs stardard)
 sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+X_train_ss = sc_X.fit_transform(X_train)
+X_test_ss = sc_X.transform(X_test)
 #sc_y = StandardScaler()
 #y_train = sc_y.fit_transform(y_train)
 
-# fit models - & DecisionTree 
-from sklearn.tree import DecisionTreeClassifier
-dt_classifier = DecisionTreeClassifier(criterion = 'gini', random_state = 0) 
-dt_classifier.fit(X_train, y_train) # 'gini' is better than 'entropy'
+mm_X = MinMaxScaler()
+X_train_mm = mm_X.fit_transform(X_train)
+X_test_mm = mm_X.transform(X_test)
 
-y_pred = dt_classifier.predict(X_test) # predict the test samples
+# fit models - Logistic Regression Models
+from sklearn.linear_model import LogisticRegression
 
-# evaluation of the model - confusion matrix
+ss_lg_classifier = LogisticRegression(random_state = 0)
+mm_lg_classifier = LogisticRegression(random_state = 0)
+
+ss_lg_classifier.fit(X_train_ss, y_train)
+mm_lg_classifier.fit(X_train_mm, y_train)
+
+# predict the test results
+ss_y_pred = ss_lg_classifier.predict(X_test)
+mm_y_pred = mm_lg_classifier.predict(X_test)
+
+# evaluation of the models
 from sklearn.metrics import confusion_matrix
-dt_confusionMatrix = confusion_matrix(y_test, y_pred)
-accRate = round(float(float(dt_confusionMatrix[0,0] + dt_confusionMatrix[1,1])/float(len(y_pred)) * 100), 2)
-print ("The accuracy of the prediction is %f percent." %accRate)
 
+cm_ss = confusion_matrix(ss_y_pred, y_test)
+cm_mm = confusion_matrix(mm_y_pred, y_test)
 
-# fit models - && RandomForest
-from sklearn.ensemble import RandomForestClassifier
-rf_classifier = RandomForestClassifier(n_estimators = 10, criterion = 'gini', random_state = 0)
-rf_classifier.fit(X_train, y_train) # 'gini' is better than 'entropy'
+ss_accrate = round(float(float(cm_ss[0,0] + cm_ss[1,1])/float(len(y_pred)) * 100), 2)
+mm_accrate = round(float(float(cm_mm[0,0] + cm_mm[1,1])/float(len(y_pred)) * 100), 2)
 
-y_pred_rf = rf_classifier.predict(X_test)
-
-# evaluation of the model - confusion matrix
-rf_confusionMatrix = confusion_matrix(y_test, y_pred_rf)
-rf_accRate = round(float(float(rf_confusionMatrix[0,0] + rf_confusionMatrix[1,1])/float(len(y_pred)) * 100), 2)
-print ("The accuracy of the prediction is %f percent." %rf_accRate)
-
-
-
-
-
-
-
-
-
-
-
-
+print ("The prediction accuracy is %f percent." %ss_accrate)
+print ("The prediction accuracy is %f percent." %mm_accrate)
 
